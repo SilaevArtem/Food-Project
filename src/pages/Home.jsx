6b9f01eeb78c49e2f71579be805df32dc9f16e62
Артём Силаев <artemsilaev@MacBook-Pro-Artem.local>
2023-03-11 21:34:30 +0300

    Add Search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,38 @@
 import { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { getAllCategories } from '../api';
 import { Preloader } from '../components/Preloader';
 import { CategoryList } from '../components/CategoryList';
+import { Search } from '../components/Search';
 
 
 function Home() {
     const [catalog, setCatalog] = useState([]);
+    const [filtredCatalog, setFiltredCatalog] = useState([]);
+    const {pathname, search} = useLocation();
+    const nav = useNavigate();
+
+    const handleSearch = (str) => {
+        setFiltredCatalog(catalog.filter(item => item.strCategory.toLowerCase().includes(str.toLowerCase())))
+        nav({
+            pathname,
+            search: `?search=${str}`
+        })
+    };
 
     useEffect(() => {
         getAllCategories().then(data => {
             setCatalog(data.categories)
+            setFiltredCatalog(search ? data.categories.filter(item => item.strCategory.toLowerCase().includes(search.split('=')[1].toLowerCase())) : data.categories)
         })
-    }, [])
+    }, [search])
     return (
         <>
+        <Search cb={handleSearch} />
         {!catalog.length ? 
             <Preloader /> 
          : (
-            <CategoryList catalog = {catalog} />
+            <CategoryList catalog = {filtredCatalog} />
         )}
         </>
     )
